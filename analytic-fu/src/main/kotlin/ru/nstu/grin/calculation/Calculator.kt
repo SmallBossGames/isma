803b5892632a71994bf30e0bb40b5e8d5fb9e1d4
kostya05983 <kostya05983@mail.ru>
2019-09-16 23:48:20 +0700

    Add class for calculations

---
+++ analytic-fu/src/main/kotlin/ru/nstu/grin/calculation/Calculator.kt
@@ -0,0 +1,35 @@
+package ru.nstu.grin.calculation
+
+import ru.nstu.grin.parser.*
+import ru.nstu.grin.parser.Number
+import java.util.*
+
+class Calculator(private val list: List<Litter>) {
+
+    fun calculate(x: Double): Double {
+        val stack = Stack<Double>()
+        list.forEach {
+            when (it) {
+                is Number -> {
+                    stack.push(it.value)
+                }
+                X -> {
+                    stack.push(x)
+                }
+                PlusOperator -> {
+                    stack.push(stack.pop() + stack.pop())
+                }
+                MinusOperator -> {
+                    stack.push(stack.pop() - stack.pop())
+                }
+                DelOperator -> {
+                    stack.push(stack.pop() / stack.pop())
+                }
+                MultiplyOperator -> {
+                    stack.push(stack.pop() * stack.pop())
+                }
+            }
+        }
+        return stack.pop()
+    }
+}

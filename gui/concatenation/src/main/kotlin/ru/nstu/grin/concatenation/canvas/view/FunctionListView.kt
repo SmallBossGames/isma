fdae18eeb701103af8106807441ebd2d473a00fb
kostya05983 <kostya05983@mail.ru>
2020-05-09 03:35:49 +0700

    Connect changeFunction modal

--- gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/canvas/view/FunctionListView.kt
+++ gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/canvas/view/FunctionListView.kt
@@ -1,19 +1,53 @@
 package ru.nstu.grin.concatenation.canvas.view
 
 import javafx.scene.Parent
+import javafx.scene.control.Tooltip
+import javafx.scene.image.Image
+import javafx.scene.image.ImageView
+import ru.nstu.grin.concatenation.axis.view.AxisChangeFragment
 import ru.nstu.grin.concatenation.canvas.controller.FunctionListViewController
 import ru.nstu.grin.concatenation.canvas.model.FunctionListViewModel
-import tornadofx.Fragment
-import tornadofx.listview
+import ru.nstu.grin.concatenation.function.view.ChangeFunctionFragment
+import tornadofx.*
 
 class FunctionListView : Fragment() {
     private val model: FunctionListViewModel by inject()
-    private val controller: FunctionListViewController = find {  }
+    private val controller: FunctionListViewController = find { }
 
     override val root: Parent = listview(model.functions) {
-
         cellFormat {
-            text = it.name
+            graphic = form {
+                hbox {
+                    spacing = 20.0
+                    fieldset("Имя") {
+                        label(it.name)
+                    }
+                    fieldset("цвет") {
+                        label(it.functionColor.toString())
+                    }
+                    fieldset("Размер линии") {
+                        label(it.lineSize.toString())
+                    }
+                    fieldset("Тип рисовки") {
+                        label(it.lineType.toString())
+                    }
+                }
+                button {
+                    action {
+                        find<ChangeFunctionFragment>(
+                            mapOf(
+                                ChangeFunctionFragment::functionId to it.id
+                            )
+                        ).openModal()
+                    }
+                    val image = Image("edit-tool.png")
+                    val imageView = ImageView(image)
+                    imageView.fitHeight = 20.0
+                    imageView.fitWidth = 20.0
+                    graphic = imageView
+                    tooltip = Tooltip("Отредактировать")
+                }
+            }
         }
     }
 }

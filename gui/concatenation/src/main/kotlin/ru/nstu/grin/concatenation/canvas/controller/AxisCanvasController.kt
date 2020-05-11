9be4ed880043a7b0a5d9d557cba403c1807e9476
kostya05983 <kostya05983@mail.ru>
2020-05-11 17:44:57 +0700

    Refactoring seperate to services.

--- gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/canvas/controller/AxisCanvasController.kt
+++ gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/canvas/controller/AxisCanvasController.kt
@@ -1,55 +1,21 @@
 package ru.nstu.grin.concatenation.canvas.controller
 
-import ru.nstu.grin.concatenation.axis.model.ConcatenationAxis
 import ru.nstu.grin.concatenation.canvas.events.*
-import ru.nstu.grin.concatenation.canvas.model.ConcatenationCanvasModelViewModel
-import ru.nstu.grin.concatenation.canvas.view.ConcatenationCanvas
+import ru.nstu.grin.concatenation.canvas.service.AxisCanvasService
 import tornadofx.Controller
-import java.util.*
 
-class AxisCanvasController: Controller() {
-    private val model: ConcatenationCanvasModelViewModel by inject()
-    private val view: ConcatenationCanvas by inject()
+class AxisCanvasController : Controller() {
+    private val service: AxisCanvasService by inject()
 
     init {
         subscribe<UpdateAxisEvent> {
-            updateAxis(it)
-            getAllAxises()
+            service.updateAxis(it)
         }
         subscribe<AxisQuery> {
-            val axis = findAxisById(it.id)
-            val event = GetAxisEvent(axis)
-            fire(event)
+            service.getAxis(it.id)
         }
         subscribe<GetAllAxisQuery> {
-            getAllAxises()
+            service.getAllAxises()
         }
-    }
-
-    private fun getAllAxises() {
-        val axises = model.cartesianSpaces.map {
-            listOf(it.xAxis, it.yAxis)
-        }.flatten()
-        val event = GetAllAxisesEvent(axises)
-        fire(event)
-    }
-
-    private fun findAxisById(id: UUID): ConcatenationAxis {
-        return model.cartesianSpaces.map {
-            listOf(it.xAxis, it.yAxis)
-        }.flatten().first { it.id == id }
-    }
-
-    private fun updateAxis(event: UpdateAxisEvent) {
-        val axis = model.cartesianSpaces.map {
-            listOf(it.xAxis, it.yAxis)
-        }.flatten().first { it.id == event.id }
-
-        axis.distanceBetweenMarks = event.distance
-        axis.textSize = event.textSize
-        axis.font = event.font
-        axis.fontColor = event.fontColor
-        axis.backGroundColor = event.axisColor
-        view.redraw()
     }
 }
--- gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/canvas/controller/CartesianCanvasController.kt
+++ gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/canvas/controller/CartesianCanvasController.kt
@@ -1,59 +1,24 @@
 package ru.nstu.grin.concatenation.canvas.controller
 
 import ru.nstu.grin.concatenation.canvas.events.*
-import ru.nstu.grin.concatenation.canvas.model.ConcatenationCanvasModelViewModel
-import ru.nstu.grin.concatenation.canvas.view.ConcatenationCanvas
+import ru.nstu.grin.concatenation.canvas.service.CartesianCanvasService
 import tornadofx.Controller
 
 class CartesianCanvasController : Controller() {
-    private val model: ConcatenationCanvasModelViewModel by inject()
-    private val view: ConcatenationCanvas by inject()
+    private val service: CartesianCanvasService by inject()
 
     init {
         subscribe<UpdateCartesianEvent> {
-            updateCartesian(it)
-            getAllCartesianSpaces()
+            service.updateCartesian(it)
         }
         subscribe<CartesianQuery> { event ->
-            val cartesianSpace = model.cartesianSpaces.first { it.id == event.id }
-            fire(GetCartesianEvent(cartesianSpace))
+            service.getCartesian(event)
         }
         subscribe<GetAllCartesiansQuery> {
-            getAllCartesianSpaces()
+            service.getAllCartesianSpaces()
         }
         subscribe<DeleteCartesianSpaceQuery> {
-            deleteCartesianSpace(it)
-            refreshDependencies()
+            service.deleteCartesianSpace(it)
         }
-    }
-
-    private fun getAllCartesianSpaces() {
-        val event = GetAllCartesiansEvent(model.cartesianSpaces)
-        fire(event)
-    }
-
-    private fun refreshDependencies() {
-        getAllCartesianSpaces()
-        val functions = model.cartesianSpaces.map { it.functions }.flatten()
-        val functionEvent = GetAllFunctionsEvent(functions)
-        fire(functionEvent)
-
-        val axises = model.cartesianSpaces.map { listOf(it.xAxis, it.yAxis) }.flatten()
-        val axisEvent = GetAllAxisesEvent(axises)
-        fire(axisEvent)
-    }
-
-    private fun updateCartesian(event: UpdateCartesianEvent) {
-        val cartesian = model.cartesianSpaces.first { it.id == event.id }
-        cartesian.isShowGrid = event.isShowGrid
-        cartesian.name = event.name
-        view.redraw()
-    }
-
-    private fun deleteCartesianSpace(event: DeleteCartesianSpaceQuery) {
-        model.cartesianSpaces.removeIf {
-            it.id == event.id
-        }
-        view.redraw()
     }
 }

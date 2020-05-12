69bcb83665e63a9dc6d2c59c48af74cb34f675bf
kostya05983 <kostya05983@mail.ru>
2020-05-13 03:23:29 +0700

    Add math module.

---
+++ math/src/main/kotlin/ru/nstu/grin/math/IntersectionSearcher.kt
@@ -0,0 +1,9 @@
+package ru.nstu.grin.math
+
+import ru.nstu.grin.model.Function
+
+class IntersectionSearcher {
+    fun findIntersections(first: Function, second: Function): List<Pair<Double, Double>> {
+        TODO("Not implemented")
+    }
+}
---
+++ math/src/main/kotlin/ru/nstu/grin/model/Function.kt
@@ -0,0 +1,5 @@
+package ru.nstu.grin.model
+
+data class Function(
+    val points: List<Pair<Double, Double>>
+)

486720fb7aa4bb482e51b1405ade132a8ecbdec3
kostya05983 <kostya05983@mail.ru>
2020-05-31 14:18:58 +0700

    Remove operations.

--- operations/src/main/kotlin/ru/nstu/grin/operations/binary/ScaleToOne.kt
+++
@@ -1,7 +0,0 @@
-package ru.nstu.grin.operations.binary
-
-class ScaleToOne : BinaryOperation {
-    override fun execute(x1: Double, x2: Double): Pair<Double, Double> {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
-    }
-}
--- operations/src/main/kotlin/ru/nstu/grin/operations/unary/ScaleOperation.kt
+++
@@ -1,8 +0,0 @@
-package ru.nstu.grin.operations.unary
-
-class ScaleOperation : UnaryOperation {
-
-    override fun execute(x: Double): Double {
-        TODO("not implemented") // To change body of created functions use File | Settings | File Templates.
-    }
-}

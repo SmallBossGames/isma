3cc9b55250bd3da157f842634f693e9906b6bbe6
Алексей Гардер <aleksey_garder@outlook.com>
2021-04-03 22:44:20 +0700

    Added text editor text

--- isma-app-tornadofx/src/main/kotlin/views/editors/blueprint/controls/StateTransactionArrow.kt
+++ isma-app-tornadofx/src/main/kotlin/views/editors/blueprint/controls/StateTransactionArrow.kt
@@ -1,43 +1,104 @@
 package views.editors.blueprint.controls
 
+import javafx.scene.control.TextField
+import javafx.scene.control.Label
+import javafx.beans.property.SimpleBooleanProperty
 import javafx.beans.property.SimpleDoubleProperty
+import javafx.beans.property.SimpleStringProperty
+import javafx.geometry.Pos
 import javafx.scene.Parent
+import javafx.scene.input.MouseEvent
+import org.w3c.dom.Text
+
 import tornadofx.*
 import kotlin.math.*
 
 class StateTransactionArrow : Fragment() {
+
     private val startXProperty = SimpleDoubleProperty(0.0)
     private val startYProperty = SimpleDoubleProperty(0.0)
     private val endXProperty = SimpleDoubleProperty(0.0)
     private val endYProperty = SimpleDoubleProperty(0.0)
+
+    private val textProperty = SimpleStringProperty("")
+    private val isTextEditModeProperty = SimpleBooleanProperty(false)
 
     fun startXProperty() = startXProperty
     fun startYProperty() = startYProperty
     fun endXProperty() = endXProperty
     fun endYProperty() = endYProperty
 
+    fun textProperty() = textProperty
+
     val startX by startXProperty
     val startY by startYProperty
     val endX by endXProperty
     val endY by endYProperty
+
+    var isTextEditMode by isTextEditModeProperty
+    var text by textProperty
 
     override val root: Parent = group {
         viewOrder = 4.0
         layoutXProperty().bind((endXProperty - startXProperty) / 2 + startXProperty)
         layoutYProperty().bind((endYProperty - startYProperty) / 2 + startYProperty)
 
+        val arrowTextEditor = TextField().apply {
+            prefWidth = TextFieldLength
+            translateY -= 11.0
+            translateX = -TextFieldLength / 2.0
+            textProperty().bindBidirectional(this@StateTransactionArrow.textProperty)
+            visibleWhen(isTextEditModeProperty)
+            managedWhen(isTextEditModeProperty)
+            println(height)
+
+            focusedProperty().onChange {
+                if(!it) {
+                    isTextEditMode = false
+                }
+            }
+        }
+        val arrowText = Label().apply {
+            prefWidth = TextFieldLength
+            translateY -= 11.0
+            translateX = -TextFieldLength / 2.0
+            alignment = Pos.CENTER
+            textProperty().bind(this@StateTransactionArrow.textProperty)
+            visibleWhen(!isTextEditModeProperty)
+            managedWhen(!isTextEditModeProperty)
+        }
+        val predicateText = group {
+            viewOrder = 7.0
+            add(arrowTextEditor)
+            add(arrowText)
+        }
+
+        val arrowhead = polyline(-5.0, -5.0, 0.0, 0.0, -5.0, 5.0) {
+            viewOrder = 6.0
+        }
+
         line {
+            viewOrder = 6.0
             fun ss() {
                 val x = this.endX - this.startX
                 val y = this.endY - this.startY
                 val angle = atan2(x, y) + PI/2
                 val offsetX = 10.0 * sin(angle)
                 val offsetY = 10.0 * cos(angle)
+                val textOffsetX = 30.0 * sin(angle)
+                val textOffsetY = 30.0 * cos(angle)
 
                 startX = this@StateTransactionArrow.startX - this@group.layoutX + offsetX
                 startY = this@StateTransactionArrow.startY - this@group.layoutY + offsetY
                 endX = this@StateTransactionArrow.endX - this@group.layoutX + offsetX
                 endY = this@StateTransactionArrow.endY - this@group.layoutY + offsetY
+
+                arrowhead.translateX = offsetX
+                arrowhead.translateY = offsetY
+                arrowhead.rotate = - angle / PI * 180.0
+
+                predicateText.translateX = textOffsetX
+                predicateText.translateY = textOffsetY
             }
 
             this@StateTransactionArrow.startXProperty.onChange { ss() }
@@ -45,5 +106,14 @@ class StateTransactionArrow : Fragment() {
             this@StateTransactionArrow.endXProperty.onChange { ss() }
             this@StateTransactionArrow.endYProperty.onChange { ss() }
         }
+
+        addEventHandler(MouseEvent.MOUSE_CLICKED) {
+            isTextEditMode = true
+            arrowTextEditor.requestFocus()
+        }
+    }
+
+    companion object {
+        private const val TextFieldLength = 200.0
     }
 }

cd9c17c2d15e8aba6af118237381506c06c78ce2
Алексей Гардер <aleksey_garder@outlook.com>
2021-04-03 21:23:51 +0700

    Implemented two-arrows configuration

--- isma-app-tornadofx/src/main/kotlin/views/editors/blueprint/controls/StateTransactionArrow.kt
+++ isma-app-tornadofx/src/main/kotlin/views/editors/blueprint/controls/StateTransactionArrow.kt
@@ -10,12 +10,6 @@ class StateTransactionArrow : Fragment() {
     private val startYProperty = SimpleDoubleProperty(0.0)
     private val endXProperty = SimpleDoubleProperty(0.0)
     private val endYProperty = SimpleDoubleProperty(0.0)
-//    private val centerXProperty = (endXProperty - startXProperty) / 2 + startXProperty
-//    private val centerYProperty = (endYProperty - startYProperty) / 2 + startYProperty
-
-    private val offsetXProperty = SimpleDoubleProperty(0.0)
-    private val offsetYProperty = SimpleDoubleProperty(0.0)
-
 
     fun startXProperty() = startXProperty
     fun startYProperty() = startYProperty
@@ -27,46 +21,29 @@ class StateTransactionArrow : Fragment() {
     val endX by endXProperty
     val endY by endYProperty
 
-    private val originalStartX = SimpleDoubleProperty()
-    private val originalStartY = SimpleDoubleProperty()
-    private val originalEndX = SimpleDoubleProperty()
-    private val originalEndY = SimpleDoubleProperty()
-
-
-
     override val root: Parent = group {
         viewOrder = 4.0
         layoutXProperty().bind((endXProperty - startXProperty) / 2 + startXProperty)
         layoutYProperty().bind((endYProperty - startYProperty) / 2 + startYProperty)
 
         line {
-            originalStartX.bind(this@StateTransactionArrow.startXProperty - this@group.layoutXProperty())
-            originalStartY.bind(this@StateTransactionArrow.startYProperty - this@group.layoutYProperty())
-            originalEndX.bind(this@StateTransactionArrow.endXProperty - this@group.layoutXProperty())
-            originalEndY.bind(this@StateTransactionArrow.endYProperty - this@group.layoutYProperty())
+            fun ss() {
+                val x = this.endX - this.startX
+                val y = this.endY - this.startY
+                val angle = atan2(x, y) + PI/2
+                val offsetX = 10.0 * sin(angle)
+                val offsetY = 10.0 * cos(angle)
 
-            originalStartX.onChange { startX = it + calcAngleX(this) }
-            originalStartY.onChange { startY = it + calcAngleY(this) }
-            originalEndX.onChange { endX = it + calcAngleX(this) }
-            originalEndY.onChange { endY = it + calcAngleY(this) }
+                startX = this@StateTransactionArrow.startX - this@group.layoutX + offsetX
+                startY = this@StateTransactionArrow.startY - this@group.layoutY + offsetY
+                endX = this@StateTransactionArrow.endX - this@group.layoutX + offsetX
+                endY = this@StateTransactionArrow.endY - this@group.layoutY + offsetY
+            }
+
+            this@StateTransactionArrow.startXProperty.onChange { ss() }
+            this@StateTransactionArrow.startYProperty.onChange { ss() }
+            this@StateTransactionArrow.endXProperty.onChange { ss() }
+            this@StateTransactionArrow.endYProperty.onChange { ss() }
         }
-    }
-
-    private fun calcAngleX(line: javafx.scene.shape.Line) : Double {
-        val x = line.endX - line.startX
-        val y = line.endY - line.startY
-
-        val angle = atan2(x, y) + PI/2
-
-        return 10.0 * sin(angle)
-    }
-
-    private fun calcAngleY(line: javafx.scene.shape.Line) : Double {
-        val x = line.endX - line.startX
-        val y = line.endY - line.startY
-
-        val angle = atan2(x, y) + PI/2
-
-        return 10.0 * cos(angle)
     }
 }

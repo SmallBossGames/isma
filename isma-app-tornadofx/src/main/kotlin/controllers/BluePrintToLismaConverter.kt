f4a6276352fe218cdfc428657df3376a15d5c74b
Алексей Гардер <aleksey_garder@outlook.com>
2021-05-15 15:38:43 +0700

    Created BluePrintToLismaConverter.kt

---
+++ isma-app-tornadofx/src/main/kotlin/controllers/BluePrintToLismaConverter.kt
@@ -0,0 +1,7 @@
+package controllers
+
+import tornadofx.Controller
+
+class BluePrintToLismaConverter : Controller() {
+
+}
--- isma-app-tornadofx/src/main/kotlin/views/IsmaEditorTabPane.kt
+++ isma-app-tornadofx/src/main/kotlin/views/IsmaEditorTabPane.kt
@@ -1,22 +1,12 @@
 package views
 
 import controllers.ActiveProjectController
-import controllers.LismaPdeController
 import controllers.ProjectController
-import controllers.SyntaxHighlightingController
-import events.*
-import javafx.beans.property.SimpleDoubleProperty
-import javafx.scene.paint.Color
-import javafx.scene.text.FontPosture
-import javafx.scene.text.FontWeight
-import org.fxmisc.richtext.CodeArea
-import org.fxmisc.richtext.LineNumberFactory
-import tornadofx.*
-
-import javafx.scene.input.MouseEvent
-import javafx.scene.shape.Rectangle
-import javafx.scene.text.TextAlignment
-import models.IsmaProjectModel
+import events.NewBlueprintProjectEvent
+import events.NewProjectEvent
+import tornadofx.View
+import tornadofx.tab
+import tornadofx.tabpane
 import views.editors.blueprint.IsmaBlueprintEditor
 import views.editors.text.IsmaTextEditor
 

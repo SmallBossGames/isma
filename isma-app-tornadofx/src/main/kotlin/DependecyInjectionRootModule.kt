d365d9b1edb5d17b76088ea1d8454d9d6314d097
Алексей Гардер <aleksey_garder@outlook.com>
2021-07-27 21:20:15 +0700

    Views uses TornadoFX DI

--- isma-app-tornadofx/src/main/kotlin/DependecyInjectionRootModule.kt
+++ isma-app-tornadofx/src/main/kotlin/DependecyInjectionRootModule.kt
@@ -1,10 +1,8 @@
 import org.koin.core.context.GlobalContext.startKoin
 import services.external.externalServicesModule
 import services.servicesModule
-import views.viewsModule
 
 fun ismaKoinStart() = startKoin {
     modules(servicesModule)
     modules(externalServicesModule)
-    modules(viewsModule)
 }

de32678e1ef6777d7d0fa59d7edef96bf16af863
Алексей Гардер <aleksey_garder@outlook.com>
2021-08-21 10:54:18 +0700

    Added java module-info.java for intg core

---
+++ isma-intg-api/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module isma.isma.intg.api.main {
+    exports ru.nstu.isma.intg.api.methods;
+    exports ru.nstu.isma.intg.api.solvers;
+    exports ru.nstu.isma.intg.api.calcmodel;
+    exports ru.nstu.isma.intg.api;
+    exports ru.nstu.isma.intg.api.models;
+    exports ru.nstu.isma.intg.api.calcmodel.cauchy;
+}
---
+++ isma-intg-core/src/main/java/module-info.java
@@ -0,0 +1,5 @@
+module isma.isma.intg.core.main {
+    requires org.apache.commons.lang3;
+    requires org.slf4j;
+    requires isma.isma.intg.api.main;
+}

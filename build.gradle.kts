41f147eb90e3f96a983f6fcd63c484b2232e8bc2
Алексей Гардер <aleksey_garder@outlook.com>
2021-10-09 17:30:50 +0700

    Added material icons

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     kotlin("jvm") version "1.5.31" apply false
     id("org.openjfx.javafxplugin") version "0.0.10" apply false
     id("com.github.johnrengelman.shadow") version "7.0.0" apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "1.5.31" apply false
     id("ch.tutteli.gradle.plugins.kotlin.module.info") version "4.0.2" apply false
     id("de.jjohannes.extra-java-module-info") version "0.9" apply false
 }

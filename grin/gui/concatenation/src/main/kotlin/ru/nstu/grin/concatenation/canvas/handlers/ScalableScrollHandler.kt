16174201b9f2e1940f8f829742c2338fd29bc2d0
Алексей Гардер <aleksey_garder@outlook.com>
2022-02-19 14:08:47 +0700

    Implemented relative chart scaling

--- grin/gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/canvas/handlers/ScalableScrollHandler.kt
+++ grin/gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/canvas/handlers/ScalableScrollHandler.kt
@@ -2,14 +2,17 @@ package ru.nstu.grin.concatenation.canvas.handlers
 
 import javafx.event.EventHandler
 import javafx.scene.input.ScrollEvent
-import ru.nstu.grin.concatenation.canvas.view.ConcatenationChainDrawer
 import ru.nstu.grin.concatenation.axis.model.ConcatenationAxis
+import ru.nstu.grin.concatenation.axis.model.Direction
+import ru.nstu.grin.concatenation.canvas.controller.MatrixTransformerController
 import ru.nstu.grin.concatenation.canvas.model.ConcatenationCanvasModel
+import ru.nstu.grin.concatenation.canvas.view.ConcatenationChainDrawer
 import tornadofx.Controller
 
 class ScalableScrollHandler : EventHandler<ScrollEvent>, Controller() {
     private val model: ConcatenationCanvasModel by inject()
     private val chainDrawer: ConcatenationChainDrawer by inject()
+    private val matrixTransformer: MatrixTransformerController by inject()
 
     override fun handle(event: ScrollEvent) {
         val axes = model.cartesianSpaces.map {
@@ -36,12 +39,19 @@ class ScalableScrollHandler : EventHandler<ScrollEvent>, Controller() {
         val oldMin = axis.settings.min
         val oldMax = axis.settings.max
 
+        val units = when(axis.direction){
+            Direction.LEFT, Direction.RIGHT ->
+                matrixTransformer.transformPixelToUnits(event.y, axis.settings, axis.direction)
+            Direction.TOP, Direction.BOTTOM ->
+                matrixTransformer.transformPixelToUnits(event.x, axis.settings, axis.direction)
+        }
+
         if(delta > 0) {
-            axis.settings.min = oldMin * (1.0 + delta * AXIS_SCALING_COEFFICIENT)
-            axis.settings.max = oldMax * (1.0 - delta * AXIS_SCALING_COEFFICIENT)
+            axis.settings.min = oldMin * (1.0 - delta * AXIS_SCALING_COEFFICIENT) + units * delta * AXIS_SCALING_COEFFICIENT
+            axis.settings.max = oldMax * (1.0 - delta * AXIS_SCALING_COEFFICIENT) + units * delta * AXIS_SCALING_COEFFICIENT
         } else {
-            axis.settings.min = oldMin / (1.0 - delta * AXIS_SCALING_COEFFICIENT)
-            axis.settings.max = oldMax / (1.0 + delta * AXIS_SCALING_COEFFICIENT)
+            axis.settings.min = (oldMin + units * delta * AXIS_SCALING_COEFFICIENT) / (1.0 + delta * AXIS_SCALING_COEFFICIENT)
+            axis.settings.max = (oldMax + units * delta * AXIS_SCALING_COEFFICIENT) / (1.0 + delta * AXIS_SCALING_COEFFICIENT)
         }
     }
 

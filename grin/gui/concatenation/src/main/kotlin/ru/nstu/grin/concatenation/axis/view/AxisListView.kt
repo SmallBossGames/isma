87be72cce90803ffbd2cf6947f56ce7a854bae9c
Алексей Гардер <aleksey_garder@outlook.com>
2022-02-23 11:47:22 +0700

    Refactor axis list view

--- grin/gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/axis/view/AxisListView.kt
+++ grin/gui/concatenation/src/main/kotlin/ru/nstu/grin/concatenation/axis/view/AxisListView.kt
@@ -3,82 +3,78 @@ package ru.nstu.grin.concatenation.axis.view
 import javafx.geometry.Insets
 import javafx.scene.Parent
 import javafx.scene.Scene
-import javafx.scene.control.Label
-import javafx.scene.control.Tooltip
+import javafx.scene.control.*
 import javafx.scene.image.Image
 import javafx.scene.image.ImageView
-import javafx.scene.layout.Background
-import javafx.scene.layout.BackgroundFill
-import javafx.scene.layout.CornerRadii
+import javafx.scene.layout.*
 import javafx.scene.text.Font
 import javafx.stage.Modality
 import javafx.stage.Stage
 import ru.nstu.grin.concatenation.axis.controller.AxisListViewController
-import ru.nstu.grin.concatenation.axis.events.GetAllAxisQuery
 import ru.nstu.grin.concatenation.axis.model.AxisListViewModel
-import tornadofx.*
+import ru.nstu.grin.concatenation.axis.model.ConcatenationAxis
+import tornadofx.Fragment
 
 class AxisListView : Fragment() {
     private val model: AxisListViewModel by inject()
     private val controller: AxisListViewController = find { }
 
-    override val root: Parent = listview(model.axises) {
-        cellFormat {
-            graphic = borderpane {
-                left {
-                    hbox {
-                        spacing = 10.0
+    override val root: Parent = ListView(model.axises).apply {
+        setCellFactory {
+            object : ListCell<ConcatenationAxis>() {
+                override fun updateItem(item: ConcatenationAxis?, empty: Boolean) {
+                    super.updateItem(item, empty)
 
-                        add(Label(it.name).apply {
-                            textFill = it.fontColor
-                            font = Font(it.font, it.textSize)
-                            background = Background(
-                                BackgroundFill(
-                                    it.backGroundColor,
-                                    CornerRadii(0.0),
-                                    Insets(0.0)
-                                )
-                            )
-                            padding = Insets(5.0)
-                        })
-
-                        add(Label("Direction: ${it.direction}").apply {
-                            padding = Insets(5.0)
-                        })
-                    }
-                }
-
-                right {
-                    button {
-                        action {
-                            val view = find<AxisChangeFragment>(
-                                mapOf(
-                                    AxisChangeFragment::axisId to it.id
-                                )
-                            )
-
-                            Stage().apply {
-                                scene = Scene(view.root)
-                                title = "Change axis"
-                                initModality(Modality.WINDOW_MODAL)
-                                initOwner(currentWindow!!)
-
-                                show()
-                            }
-                        }
-                        val image = Image("edit-tool.png")
-                        val imageView = ImageView(image)
-                        imageView.fitHeight = 20.0
-                        imageView.fitWidth = 20.0
-                        graphic = imageView
-                        tooltip = Tooltip("Отредактировать")
-                    }
+                    graphic = if (item == null) null else createItem(item)
                 }
             }
         }
     }
 
-    init {
-        fire(GetAllAxisQuery())
+    private fun createItem(item: ConcatenationAxis): BorderPane {
+        return BorderPane().apply {
+            left = HBox(
+                Label(item.name).apply {
+                    textFill = item.fontColor
+                    font = Font(item.font, item.textSize)
+                    background = Background(
+                        BackgroundFill(
+                            item.backGroundColor,
+                            CornerRadii(0.0),
+                            Insets(0.0)
+                        )
+                    )
+                    padding = Insets(5.0)
+                },
+                Label("Direction: ${item.direction}").apply {
+                    padding = Insets(5.0)
+                }
+            ).apply {
+                spacing = 10.0
+            }
+
+            right = Button(null, ImageView(Image("edit-tool.png")).apply {
+                fitHeight = 20.0
+                fitWidth = 20.0
+            }).apply {
+                tooltip = Tooltip("Отредактировать")
+                setOnAction {
+                    val view = find<AxisChangeFragment>(
+                        mapOf(
+                            AxisChangeFragment::axisId to item.id
+                        )
+                    )
+
+                    Stage().apply {
+                        scene = Scene(view.root)
+                        title = "Change axis"
+                        initModality(Modality.WINDOW_MODAL)
+                        initOwner(currentWindow!!)
+
+                        show()
+                    }
+                }
+            }
+        }
     }
 }
